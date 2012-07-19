e94c892125309052980230b715b9f6d1f0d3f926
jed <jed@jed.com>
2012-07-18 18:06:38 -0700

    slammer

---
+++ js/slammer.js
@@ -0,0 +1,29 @@
+/*
+needs to:
+- send money back and forth between two accounts as fast as possible for 10 min
+- make, take, remove offers from one 
+
+*/
+var slammer={};
+
+slammer.send=function()
+{
+	toAccount=$.trim( $("#SendDest").val() );
+	
+	currency= $("#SendCurrency").parent().children()[1].value.substring(0,3).toUpperCase();
+	if(currency=='XNS') amount=''+$.trim( $("#SendAmount").val() )*BALANCE_DISPLAY_DIVISOR;
+	else amount=''+$.trim( $("#SendAmount").val() );
+	
+	
+	for(var n=0; n<10000; n++)
+	{
+		if(n%1000==0)console.log("At: "+n);
+		
+		rpc.send(ncc.masterKey, ncc.accountID, toAccount, amount, currency, slammer.onSendResponse);	
+	}
+}
+
+send.onSendResponse = function(response,success)
+{
+	
+}
