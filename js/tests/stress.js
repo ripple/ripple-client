6467eef0478e809974f201d392c12982919e0a32
jed <jed@jed.com>
2012-09-30 18:29:39 -0700

    stress working

--- js/tests/stress.js
+++ js/tests/stress.js
@@ -4,6 +4,16 @@
 var stressUI={};
 stressUI.createAccounts=function()
 {
+	var num=$("#NumStressAccounts").val();
+	console.log("Creating "+num+" accounts");
+	stress.createAccounts(num);
+}
+
+stressUI.doSends=function()
+{
+	var num=$("#NumStressSends").val();
+	console.log("Creating "+num+" accounts");
+	stress.doSends(num);
 }
 
 var stress={};
@@ -14,8 +24,8 @@ stress.startUp=function()
 {
 	var newAccount={};
 	newAccount.master_seed="masterpassphrase"
-    newAccount.account_id=response.result.account_id;
-    newAccount.balance=100000000000000000;
+    newAccount.account_id="iHb9CJAWyB4ij91VRWn96DkukG4bwdtyTh";
+    newAccount.balance=100000000000000;
     
 	stress.accounts[0]=newAccount;
 }
@@ -37,12 +47,12 @@ stress.createCB = function (response, success, numberToAdd)
 	{
 		ncc.checkError(response);
 		response.result
-		sender=stress.getRandomAccount();
 		
-		var n=Math.rand(stress.accounts.length);
+		var n=Math.floor(Math.random()*stress.accounts.length);
 		
-		var amount=Math.rand()*stress.accounts[n].balance;
-		if(amount>99999999)amount=99999999;
+		var amount=Math.floor(Math.random()*stress.accounts[n].balance);
+		if(amount>99999)amount=99999;
+		
 		
 		stress.accounts[n].balance -= amount;
 		
@@ -52,7 +62,11 @@ stress.createCB = function (response, success, numberToAdd)
         newAccount.balance=amount;
         
 		stress.accounts[stress.accounts.length]=newAccount;
-		rpc.send(stress.accounts[n].master_seed,stress.accounts[n].account_id,newAccount.account_id,amount,"XNS",
+		
+		amount *= BALANCE_DISPLAY_DIVISOR;
+		console.log("Sending:"+stress.accounts[n].master_seed+" "+stress.accounts[n].account_id+" "+newAccount.account_id+" "+amount+" XNS");
+		
+		rpc.send(stress.accounts[n].master_seed,stress.accounts[n].account_id,newAccount.account_id,""+amount,"XNS",
 				function(response, success){ stress.sendCB(response, success, numberToAdd); });
 	}else ncc.serverDown();
 	
@@ -70,9 +84,45 @@ stress.sendCB = function (response, success, numberToAdd)
   } else ncc.serverDown();
 }
 
+stress.doSendsCB = function (response, success, numberToAdd)
+{
+  if(success) 
+  {
+    ncc.checkError(response);
+    stress.doSends(numberToAdd-1);
+    
+  } else ncc.serverDown();
+}
 
+//this will send between random accounts
+stress.doSends=function(numberOfSends)
+{
+	if(numberOfSends>0)
+	{
+		var i=Math.floor(Math.random()*stress.accounts.length);
+		var j=Math.floor(Math.random()*stress.accounts.length);
+		
+		var amount=Math.floor(Math.random()*stress.accounts[i].balance);
+		if(amount>99999)amount=99999;
+		
+		
+		stress.accounts[i].balance -= amount;
+		stress.accounts[j].balance += amount;
+		
+		
+		
+		amount *= BALANCE_DISPLAY_DIVISOR;
+		console.log("Sending:"+stress.accounts[i].master_seed+" "+stress.accounts[i].account_id+" "+stress.accounts[j].account_id+" "+amount+" XNS");
+		
+		rpc.send(stress.accounts[i].master_seed,stress.accounts[i].account_id,stress.accounts[j].account_id,""+amount,"XNS",
+				function(response, success){ stress.doSendsCB(response, success, numberOfSends); });
+	}
+}
+
+
+/*
 stress.getRandomAccount=function()
 {
 	var n=Math.rand(stress.accounts.length);
 	return(stressTest.accounts[n]);
-}
+}*/
