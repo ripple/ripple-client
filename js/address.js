a883adb715d8abfc7fcc99617b936a7494e13ccd
Andrey Fedorov <anfedorov@gmail.com>
2012-10-15 13:59:36 -0400

    added ability to add entries on address book page

--- js/address.js
+++ js/address.js
@@ -1,21 +1,58 @@
 var AddressBookPage = new (function () {
+  var AddressBookTable = $("#AddressBookTable"),
+      
+      populatedRow = function (name, addr) {
+        return ('<tr data-name="' + name + '" data-addr="' + addr + '">' +
+                  '<td class="addr">' + addr + '</td>' +
+                  '<td class="name">' + name + '</td>' +
+                  '<td class="edit">' +
+                    '<button class="edit" onclick="AddressBookPage.editRow(this.parentElement.parentElement)">edit</button>' +
+                    '<button class="save" onclick="AddressBookPage.saveRow(this.parentElement.parentElement)">save</button>' +
+                  '</td>' +
+                '</tr>');
+      },
+      
+      newRow = ('<tr data-name="" data-addr="">' +
+                  '<td class="addr"><input name="addr" /></td>' +
+                  '<td class="name"><input name="name" /></td>' +
+                  '<td class="edit">' +
+                    '<button class="edit" onclick="AddressBookPage.editRow(this.parentElement.parentElement)">edit</button>' +
+                    '<button class="save" onclick="AddressBookPage.saveRow(this.parentElement.parentElement)">save</button>' +
+                  '</td>' +
+                '</tr>'),
+      
+      addRow = ('<tr>' +
+                  '<td class="addr">&nbsp;</td>' +
+                  '<td class="name">&nbsp;</td>' +
+                  '<td class="edit">' +
+                    '<button class="add" onclick="AddressBookPage.newRow()">add</button>' +
+                  '</td>' +
+                '</tr>');
+  
   this.onShowTab = function () {
-    $("#AddressBookTable").html("");
+    AddressBookTable.html('');
+    
     _.each(
       blobVault.addressBook.getEntries(),
       function (name, addr) {
-        $("#AddressBookTable").append(
-          '<tr data-name="' + name + '" data-addr="' + addr + '">' +
-            '<td class="addr">' + addr + '</td>' +
-            '<td class="name">' + name + '</td>' +
-            '<td class="edit">' +
-              '<button class="edit" onclick="AddressBookPage.editRow(this.parentElement.parentElement)">edit</button>' +
-              '<button class="save" onclick="AddressBookPage.saveRow(this.parentElement.parentElement)">save</button>' +
-            '</td>' +
-          '</tr>'
-        );
+        AddressBookTable.append(populatedRow(name, addr));
       }
     );
+    
+    AddressBookTable.append(addRow);
+  };
+  
+  this.newRow = function () {
+    var row = $(newRow),
+        editButton = row.find('button.edit'),
+        saveButton = row.find('button.save');
+    
+    AddressBookTable.find('tr:last').before(row);
+    editButton.hide();
+    saveButton.show();
+    
+    row.find('input').keydown(function (e) { if (e.which == 13) saveButton.click(); });
+    row.find('input:first').focus();
   };
   
   this.editRow = function (rowElem) {
@@ -26,22 +63,21 @@ var AddressBookPage = new (function () {
     editButton.hide();
     saveButton.show();
     
-    row.find('td.name').html($('<input>').val(row.attr('data-name')));
-    row.find('td.name input').focus().keydown(function (e) {
-      if (e.which == 13) saveButton.click();
-    });
+    row.find('td.name').html($('<input name="name">').val(row.attr('data-name')));
+    row.find('td.name input').focus().keydown(function (e) { if (e.which == 13) saveButton.click(); });
   }
   
   this.saveRow = function (rowElem) {
     var row = $(rowElem),
         oldName = row.attr('data-name'),
         newName = row.find('td.name input').val(),
-        addr = row.attr('data-addr');
+        addr = row.find('td.addr input').val() || row.attr('data-addr');
     
     if (newName) {
       row.find('button.save').hide();
       row.find('button.edit').show();
       row.find('td.name').text(newName);
+      row.find('td.addr').text(addr);
       row.attr('data-name', newName);
     } else {
       row.remove();
