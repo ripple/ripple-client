16938a17d1d0e59544c1313a775fada1f00a6a3f
Stefan Thomas <justmoon@members.fsf.org>
2012-11-26 18:59:47 -0800

    Add amount parsing and confirm page to send tab.

--- src/js/tabs/send.js
+++ src/js/tabs/send.js
@@ -1,11 +1,10 @@
 var util = require('util');
 var Tab = require('../client/tabmanager').Tab;
+var Amount = ripple.Amount;
 
 var SendTab = function ()
 {
   Tab.call(this);
-
-  this.on('afterrender', this.onAfterRender.bind(this));
 };
 
 util.inherits(SendTab, Tab);
@@ -20,24 +19,41 @@ SendTab.prototype.generateHtml = function ()
 SendTab.prototype.angular = function (module)
 {
   var app = this.app;
-  module.controller('SendCtrl', function ($scope) {
+  module.controller('SendCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.reset = function () {
+      $scope.mode = "form";
+      $scope.recipient = '';
+      $scope.amount = '';
+      $scope.currency = 'XRP';
+    };
     $scope.send = function () {
+      var amount = ripple.Amount.from_human(""+$scope.amount+" "+$scope.currency);
+
+      $scope.amount_feedback = amount.to_human();
+      $scope.currency_feedback = amount._currency.to_json();
+
+      $scope.confirm_wait = true;
+      $timeout(function () {
+        $scope.confirm_wait = false;
+        $scope.$digest();
+      }, 1000);
+
+      $scope.mode = "confirm";
+    };
+
+    $scope.send_confirmed = function () {
+      var amount = ripple.Amount.from_human(""+$scope.amount);
+
       var tx = app.net.remote.transaction();
-      tx.payment(app.id.account, $scope.recipient, ""+$scope.amount);
+      tx.payment(app.id.account, $scope.recipient, amount.to_json());
       tx.set_flags('CreateAccount');
       tx.on('success', function () {});
       tx.on('error', function () {});
       tx.submit();
     };
-  });
-};
 
-SendTab.prototype.onAfterRender = function ()
-{
-  var self = this;
-  this.el.find('form').submit(function (e) {
-    e.preventDefault();
-  });
+    $scope.reset();
+  }]);
 };
 
 module.exports = SendTab;
