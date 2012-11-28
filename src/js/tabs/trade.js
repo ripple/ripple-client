60a093866e15b85a79f1af6c4e24e6029cf7de24
jed <jed@ripple.com>
2012-11-28 13:56:54 -0800

    trade

--- src/js/tabs/trade.js
+++ src/js/tabs/trade.js
@@ -18,12 +18,47 @@ TradeTab.prototype.angular = function (module)
 {
   module.controller('TradeCtrl', function ($scope)
   {
+    $scope.reset = function () {
+      $scope.mode = "trade";
+      $scope.order_amount = '';
+      $scope.order_price = '';
+    };
+    
     $scope.placeOrder = function () {
       var buy_amount = ripple.Amount.from_human(""+$scope.order_amount+" "+$scope.buy_currency);
       var sell_amount = ripple.Amount.from_human(""+$scope.order_amount+" "+$scope.sell_currency);
-
       
+      $scope.sell_amount_feedback = sell_amount.to_human();
+      $scope.sell_currency_feedback = sell_amount._currency.to_json();
+      $scope.buy_amount_feedback = buy_amount.to_human();
+      $scope.buy_currency_feedback = buy_amount._currency.to_json();
+      
+      $scope.mode = "confirm";
     };
+    
+    $scope.order_confirmed = function () {
+     /* var amount = ripple.Amount.from_human(""+$scope.amount);
+
+      var tx = app.net.remote.transaction();
+      tx.payment(app.id.account, $scope.recipient, amount.to_json());
+      */
+      //.offer_create("root", "500", "100/USD/root")
+      tx.on('success', function () {
+        $scope.reset();
+        $scope.$digest();
+      });
+      tx.on('error', function () {
+        $scope.mode = "error";
+        $scope.$digest();
+      });
+      tx.submit();
+
+      $scope.mode = "sending";
+    };
+
+    $scope.reset();
+    
+    
   });
 };
 
