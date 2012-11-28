7ae6c721365259ff920e8dc684bd812b817feffe
jed <jed@ripple.com>
2012-11-28 03:29:23 -0800

    trade

--- src/js/tabs/trade.js
+++ src/js/tabs/trade.js
@@ -18,6 +18,12 @@ TradeTab.prototype.angular = function (module)
 {
   module.controller('TradeCtrl', function ($scope)
   {
+    $scope.placeOrder = function () {
+      var buy_amount = ripple.Amount.from_human(""+$scope.order_amount+" "+$scope.buy_currency);
+      var sell_amount = ripple.Amount.from_human(""+$scope.order_amount+" "+$scope.sell_currency);
+
+      
+    };
   });
 };
 
