f67dbc6051d44b034f70c711011cf6dfcbd3bdfc
jed <jed@ripple.com>
2012-11-28 16:27:01 -0800

    feed

--- src/js/tabs/feed.js
+++ src/js/tabs/feed.js
@@ -14,9 +14,25 @@ FeedTab.prototype.generateHtml = function ()
   return require('../../jade/tabs/feed.jade')();
 };
 
-FeedTab.prototype.angular = function ()
+FeedTab.prototype.angular = function (module)
 {
-
+  module.controller('FeedCtrl', function ($scope)
+  { 
+    var app = this.app;
+    $scope.toggle_feed_ledger = function () 
+    {
+      console.log($scope.ledgerCheck);
+      if($scope.ledgerCheck)
+        app.net.remote.request_subscribe("ledger");
+      else app.net.remote.request_unsubscribe("ledger");
+    };
+    $scope.toggle_feed_transactions = function () {
+    };
+    $scope.toggle_feed_server = function () {
+    };
+    $scope.clear_feed = function () {
+    }; 
+  });
 };
 
 module.exports = FeedTab;
