254efe9ca20ecb79085cef1522d05e7a90db89f0
jed <jed@ripple.com>
2012-11-29 18:28:22 -0800

    feed

--- src/js/tabs/feed.js
+++ src/js/tabs/feed.js
@@ -4,6 +4,8 @@ var Tab = require('../client/tabmanager').Tab;
 var FeedTab = function ()
 {
   Tab.call(this);
+  
+  this.on('afterrender', this.onAfterRender.bind(this));
 };
 
 util.inherits(FeedTab, Tab);
@@ -14,25 +16,53 @@ FeedTab.prototype.generateHtml = function ()
   return require('../../jade/tabs/feed.jade')();
 };
 
+FeedTab.prototype.onAfterRender = function ()
+{
+  var app = this.app;
+  app.net.remote.on("net_transaction", this.handleMsg.bind(this) );
+  app.net.remote.on("net_ledger", this.handleMsg.bind(this) );
+  app.net.remote.on("net_server", this.handleMsg.bind(this) );
+};
+
 FeedTab.prototype.angular = function (module)
 {
+  var app = this.app;
   module.controller('FeedCtrl', function ($scope)
   { 
-    var app = this.app;
-    $scope.toggle_feed_ledger = function () 
+    $scope.toggle_feed_transactions = function () 
     {
-      console.log($scope.ledgerCheck);
-      if($scope.ledgerCheck)
-        app.net.remote.request_subscribe("ledger");
-      else app.net.remote.request_unsubscribe("ledger");
+      console.log($scope.transCheck);
+      if($scope.transCheck)
+        app.net.remote.request_subscribe("transactions").request();
+      else app.net.remote.request_unsubscribe("transactions").request();
     };
-    $scope.toggle_feed_transactions = function () {
+    $scope.toggle_feed_server = function () 
+    {
+      if($scope.serverCheck)
+        app.net.remote.request_subscribe("server").request();
+      else app.net.remote.request_unsubscribe("server").request();
     };
-    $scope.toggle_feed_server = function () {
-    };
-    $scope.clear_feed = function () {
+    $scope.clear_feed = function () 
+    {
     }; 
   });
 };
+
+FeedTab.prototype.handleMsg = function(message)
+{
+  console.log(message);
+  if(message.type=="net_transaction" && $scope.transCheck)
+  {
+    
+  }else if(message.type=="net_ledger" && $scope.ledgerCheck)
+  {
+    
+  }else if(message.type=="net_server" && $scope.serverCheck)
+  {
+    
+  }
+}
+
+
 
 module.exports = FeedTab;
