dec8a8a19b0487ae2baa5281056f16e6d7394c00
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-10 09:08:36 -0800

    trust page with grant trust confirmation

--- src/js/tabs/trust.js
+++ src/js/tabs/trust.js
@@ -23,29 +23,132 @@ TrustTab.prototype.angular = function (module)
   var self = this;
   var app = this.app;
 
-  module.controller('TrustCtrl', function ($scope)
+  module.controller('TrustCtrl', ['$scope', '$timeout', function ($scope, $timeout)
   {
-    /**
-     * Used for rpDestination validator
-     *
-     * @param destionation
-     */
-    $scope.recipient_query = function (match) {
-      return $scope.userBlob.data.contacts.map(function (contact) {
-        return contact.name;
-      }).filter(function (v) {
-        return v.toLowerCase().match(match.toLowerCase());
-      });
-    };
+    $scope.reset = function () {
+      $scope.mode = 'main';
+      $scope.currency = 'USD';
 
-    $scope.currency_query = webutil.queryFromOptions($scope.currencies);
-    
-    $scope.currency = 'USD';
+      $scope.$digest();
+    };
 
     $scope.toggle_form = function ()
     {
       $scope.addform_visible = !$scope.addform_visible;
     };
+
+    $scope.$watch('counterparty', function(){
+      if ($scope.contact = webutil.getContact($scope.userBlob.data.contacts,$scope.counterparty)) {
+        $scope.counterparty_name = $scope.contact.name;
+        $scope.counterparty_address = $scope.contact.address;
+      } else {
+        $scope.counterparty_name = '';
+        $scope.counterparty_address = $scope.counterparty;
+      }
+    }, true);
+
+    /**
+     * N2. Confirmation page
+     */
+    $scope.grant = function ()
+    {
+      var currency = $scope.currency.slice(0, 3).toUpperCase();
+      var amount = ripple.Amount.from_human(""+$scope.amount+" "+currency);
+
+      $scope.amount_feedback = amount.to_human();
+      $scope.currency_feedback = amount._currency.to_json();
+
+      $scope.confirm_wait = true;
+      $timeout(function () {
+        $scope.confirm_wait = false;
+        $scope.$digest();
+      }, 1000);
+
+      $scope.mode = "confirm";
+    };
+
+    /**
+     * N3. Waiting for grant result page
+     */
+    $scope.grant_confirmed = function () {
+      var currency = $scope.currency.slice(0, 3).toUpperCase();
+      var amount = $scope.amount + '/' + currency + '/' + $scope.counterparty_address;
+
+      var tx = app.net.remote.transaction();
+      tx
+        .ripple_line_set(app.id.account, amount)
+        .on('success', function(res){
+          setEngineStatus(res, false);
+          $scope.granted(this.hash);
+          $scope.$digest();
+        })
+        .on('error', function(){
+          $scope.mode = "error";
+          $scope.$digest();
+        })
+        .submit()
+      ;
+
+      $scope.mode = "granting";
+    };
+
+    /**
+     * N5. Granted page
+     */
+    $scope.granted = function (hash) {
+      $scope.mode = "granted";
+      app.net.remote.on('net_account', handleAccountEvent);
+
+      function handleAccountEvent(e) {
+        if (e.transaction.hash === hash) {
+          setEngineStatus(e, true);
+          $scope.$digest();
+          app.net.remote.removeListener('net_account', handleAccountEvent);
+        }
+      }
+    };
+
+    function setEngineStatus(res, accepted) {
+      $scope.engine_result = res.engine_result;
+      $scope.engine_result_message = res.engine_result_message;
+      switch (res.engine_result.slice(0, 3)) {
+        case 'tes':
+          $scope.tx_result = accepted ? "cleared" : "pending";
+          break;
+        case 'tem':
+          $scope.tx_result = "malformed";
+          break;
+        case 'ter':
+          $scope.tx_result = "failed";
+          break;
+        case 'tep':
+          console.warn("Unhandled engine status encountered!");
+      }
+    }
+
+    $scope.showSaveAddressForm = function () {
+      $('#saveAddressForm').slideDown();
+    }
+
+    $scope.hideSaveAddressForm = function () {
+      $('#saveAddressForm').slideUp();
+    }
+
+    $scope.saveAddress = function () {
+      $scope.addressSaving = true;
+
+      var contact = {
+        'name': $scope.saveAddressName,
+        'address': $scope.counterparty_address
+      }
+
+      app.id.once('blobsave', function(){
+        $scope.contact = contact;
+        $scope.addressSaved = true;
+      })
+
+      app.$scope.userBlob.data.contacts.unshift(contact);
+    }
 
     $scope.edit_line = function ()
     {
@@ -56,34 +159,23 @@ TrustTab.prototype.angular = function (module)
       $scope.amount = +line.limit.to_text();
     };
 
-    $scope.grant = function ()
-    {
-      var counterparty = webutil.getContact($scope.userBlob.data.contacts,$scope.counterparty).address;
-
-      if (!counterparty) {
-        counterparty = $scope.counterparty;
-      }
-
-      var currency = $scope.currency.slice(0, 3).toUpperCase();
-      var amount = $scope.amount + '/' +
-            currency + '/' +
-          counterparty;
-
-      var tx = app.net.remote.transaction();
-      tx
-        .ripple_line_set(app.id.account, amount)
-        .on('error', function(){
-          $scope.remoteError = true;
-          $scope.$digest();
-        })
-        .on('success', function(){
-          $scope.addform_visible = false;
-          $scope.$digest();
-        })
-        .submit()
-      ;
+    /**
+     * Used for rpDestination validator
+     *
+     * @param destionation
+     */
+    $scope.counterparty_query = function (match) {
+      return $scope.userBlob.data.contacts.map(function (contact) {
+        return contact.name;
+      }).filter(function (v) {
+        return v.toLowerCase().match(match.toLowerCase());
+      });
     };
-  });
+
+    $scope.currency_query = webutil.queryFromOptions($scope.currencies);
+
+    $scope.reset();
+  }]);
 };
 
 module.exports = TrustTab;
