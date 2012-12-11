4ac1835aebcf128b342746752b55d22e2d9ce3f6
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-10 17:39:52 -0800

    display username with same capitalization as how they enter it in open wallet

--- src/js/client/id.js
+++ src/js/client/id.js
@@ -48,7 +48,9 @@ Id.defaultBlob = Id.minimumBlob;
 Id.normalizeUsername = function (username) {
   username = ""+username;
   username = username.trim();
-  username = username.toLowerCase();
+// we should display username with same capitalization as how they enter it in open wallet
+// toLowerCase used in all blob requests
+// username = username.toLowerCase();
   return username;
 };
 
@@ -87,7 +89,7 @@ Id.prototype.init = function ()
     self.emit('blobupdate');
     if (self.username && self.password) {
       blob.set(self.blobBackends,
-               self.username, self.password,
+               self.username.toLowerCase(), self.password,
                self.app.$scope.userBlob,function(){
         self.emit('blobsave');
       });
@@ -170,7 +172,7 @@ Id.prototype.register = function (username, password, callback, masterkey)
   };
 
   // Add user to blob
-  blob.set(self.blobBackends, username, password, data, function () {
+  blob.set(self.blobBackends, username.toLowerCase(), password, data, function () {
     self.setUsername(username);
     self.setPassword(password);
     self.setAccount(data.data.account_id, data.data.master_seed);
@@ -192,7 +194,7 @@ Id.prototype.login = function (username,password,callback)
   username = Id.normalizeUsername(username);
   password = Id.normalizePassword(password);
 
-  blob.get(self.blobBackends, username, password, function (err, blob) {
+  blob.get(self.blobBackends, username.toLowerCase(), password, function (err, blob) {
     if (err ||
         "object" !== typeof blob.data ||
         "string" !== typeof blob.data.account_id ||
@@ -229,8 +231,8 @@ Id.prototype.logout = function ()
   this.loginStatus = false;
   this.app.tabs.gotoTab('login');
 
-  self.username = '';
-  self.password = '';
+  this.setUsername('');
+  this.setPassword('');
 
   this.app.$scope.userBlob = Id.defaultBlob;
   this.app.$scope.userCredentials = {};
