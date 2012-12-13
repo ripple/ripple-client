88a046d256a2075aa077545f6c4819f56d806aec
Stefan Thomas <justmoon@members.fsf.org>
2012-12-12 17:41:47 -0800

    Working on smarter precision handling/formatting. For now, show max. precision.

--- src/js/angular/filters.js
+++ src/js/angular/filters.js
@@ -14,7 +14,7 @@ module.filter('rpamount', function () {
     } else if ("object" !== typeof opts) {
       opts = {};
     }
-    if (!opts.precision) opts.precision = 0;
+    if (!opts.precision) opts.precision = 15;
 
     if (!input) return "n/a";
 
