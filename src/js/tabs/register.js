a186c4e57e7725e16f136e2909c0453f1a7f7170
Vahe Hovhannisyan <vhpoet@gmail.com>
2012-12-13 17:56:03 -0800

    Password strength meter

--- src/js/tabs/register.js
+++ src/js/tabs/register.js
@@ -39,6 +39,26 @@ RegisterTab.prototype.angular = function (module) {
       $scope.showMasterKeyInput = false;
     }
 
+    $scope.$watch('password1', function() {
+      if (angular.isDefined($scope.password1)) {
+        if ($scope.password1.length > 0) {
+          $scope.strength = 'weak';
+        }
+
+        if ($scope.password1.length > 5) {
+          $scope.strength = 'medium';
+        }
+
+        var regex = /(?:[A-Za-z].*?\d|\d.*?[A-Za-z])/; 
+
+        if (!!$scope.password1.match(regex) && $scope.password1.length > 8) {
+          $scope.strength = 'strong';
+        }
+      } else {
+        $scope.strength = '';
+      }
+    });
+
     $scope.register = function()
     {
       app.id.register($scope.username, $scope.password1, function(key){
