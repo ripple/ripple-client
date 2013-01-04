832d92ddacb354786a0cd296c98559c6ce90ca76
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-01-04 16:13:35 -0500

    Popup angular.js service

---
+++ src/js/services/popup.js
@@ -0,0 +1,87 @@
+/**
+ * POPUP
+ *
+ * The popup service is used to provide modals, alerts, and confirmation screens
+ */
+
+var module = angular.module('popup', []);
+
+module.factory('rpPopup', function ($http, $compile)
+{
+  var popupService = {};
+
+  // Get the popup
+  popupService.getPopup = function(create)
+  {
+    if (!popupService.popupElement && create)
+    {
+      popupService.popupElement = $( '<div class="modal hide"></div>' );
+      popupService.popupElement.appendTo( 'BODY' );
+    }
+
+    return popupService.popupElement;
+  };
+
+  popupService.compileAndRunPopup = function (popup, scope, options) {
+    $compile(popup)(scope);
+    popup.modal(options);
+  };
+
+  popupService.confirm = function(title, actionText, actionButtonText, actionFunction, cancelButtonText, cancelFunction, scope, options) {
+    actionText = (actionText) ? actionText : "Are you sure?";
+    actionButtonText = (actionButtonText) ? actionButtonText : "Ok";
+    cancelButtonText = (cancelButtonText) ? cancelButtonText : "Cancel";
+
+    var popup = popupService.getPopup(true);
+    var confirmHTML = "";
+
+    if (title) {
+      confirmHTML += "<div class=\"modal-header\"><h1>"+title+"</h1></div>";
+    }
+
+    confirmHTML += "<div class=\"modal-body\"><p class=\"question\">"+actionText+"</p>"
+        +    "<p class=\"actions\">";
+
+    if (actionFunction) {
+      confirmHTML += "<button class=\"btn btn-info btn-cancel\" ng-click=\""+actionFunction+"\">"+actionButtonText+"</button>";
+    }
+    else {
+      confirmHTML += "<button class=\"btn btn-info btn-cancel\">"+actionButtonText+"</button>";
+    }
+
+    if (cancelFunction) {
+      confirmHTML += "<a class=\"btn-cancel\" ng-click=\""+cancelFunction+"\">"+cancelButtonText+"</a>";
+    }
+    else {
+      confirmHTML += "<a class=\"btn-cancel\">"+cancelButtonText+"</a>";
+    }
+
+    confirmHTML += "</p></div>";
+
+    popup.html(confirmHTML);
+
+    if (!actionFunction) {
+      popup.find(".btn-primary").click(function () {
+        popupService.close();
+      });
+    }
+
+    if (!cancelFunction) {
+      popup.find(".btn-cancel").click(function () {
+        popupService.close();
+      });
+    }
+
+    popupService.compileAndRunPopup(popup, scope, options);
+  };
+
+  popupService.close = function()
+  {
+    var popup = popupService.getPopup();
+    if (popup) {
+      popup.modal('hide');
+    }
+  };
+
+  return popupService;
+});
