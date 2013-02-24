7c7cc16c545759d6a489688d2382796a4a40ff29
Stefan Thomas <justmoon@members.fsf.org>
2013-02-24 15:05:45 +0100

    Prevent add contact button from propagating, triggering the link to graph.

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -358,3 +358,14 @@ module.directive('rpSelectEl', [function () {
     }
   };
 }]);
+
+module.directive('rpNoPropagate', [function () {
+  return {
+    restrict: 'A',
+    link: function ($scope, element, attr) {
+      element.click(function (e) {
+        e.stopPropagation();
+      });
+    }
+  };
+}]);
