221d0810e988fab264d1f4e5747152102f76ff44
Stefan Thomas <justmoon@members.fsf.org>
2013-03-02 22:04:10 +0100

    Add unit test stubs. See #269.

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+/*
+describe('MyCtrl1', function(){
+  var myCtrl1;
+
+  beforeEach(function(){
+    myCtrl1 = new MyCtrl1();
+  });
+
+
+  it('should ....', function() {
+    //spec body
+  });
+});
+*/
