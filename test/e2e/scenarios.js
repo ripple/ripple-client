009adc21b5b692cbcfa6ff11ac4177820ff471f1
Stefan Thomas <justmoon@members.fsf.org>
2013-03-02 10:43:25 +0100

    Add end-to-end test suite. See #269.

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+
+describe('ripple client', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('../../index_debug.html');
+  });
+
+
+  it('should automatically redirect to /balance when location hash/fragment is empty', function() {
+    expect(browser().location().url()).toBe("/balance");
+  });
+
+  describe('login', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('#/login');
+    });
+
+
+    it('should render login when user navigates to /login', function() {
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/Enter the Name and Passphrase used to encrypt your Wallet below/);
+    });
+
+  });
+
+
+  describe('register', function() {
+
+    beforeEach(function() {
+      browser().navigateTo('#/register');
+    });
+
+
+    it('should render register when user navigates to /register', function() {
+      expect(element('[ng-view] p:first').text()).
+        toMatch(/Ripple is an open source p2p payment network./);
+    });
+
+  });
+});
