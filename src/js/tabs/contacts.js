f78040d0e6b41ecf554b765f1835f261d6303292
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-03-11 16:37:17 +0400

    Contacts tab: Allow destination tag to be specified. Fixes #149

--- src/js/tabs/contacts.js
+++ src/js/tabs/contacts.js
@@ -24,120 +24,124 @@ ContactsTab.prototype.angular = function (module) {
   var app = this.app;
 
   module.controller('ContactsCtrl', ['$scope', 'rpId',
-                                     function ($scope, $id)
-  {
-    if (!$id.loginStatus) return $id.goId();
-
-    $scope.reset_form = function ()
+    function ($scope, $id)
     {
-      $scope.contact = {
-        name: '',
-        address: ''
-      };
-      if ($scope.addForm) $scope.addForm.$setPristine();
-    };
+      if (!$id.loginStatus) return $id.goId();
 
-    $scope.reset_form();
-
-    /**
-     * Toggle "add contact" form
-     */
-    $scope.toggle_form = function ()
-    {
-      $scope.addform_visible = !$scope.addform_visible;
-      $scope.reset_form();
-
-      // Focus on first input
-      setImmediate(function() {
-        $('#addForm').find('input:first').focus();
-      });
-    };
-
-    /**
-     * Create contact
-     */
-    $scope.create = function ()
-    {
-      var contact = {
-        name: $scope.contact.name,
-        address: $scope.contact.address,
-        // Used for animation
-        justAdded: true
+      $scope.reset_form = function ()
+      {
+        $scope.contact = {
+          name: '',
+          address: '',
+          dt: ''
+        };
+        if ($scope.addForm) $scope.addForm.$setPristine();
       };
 
-      // Enable the animation
-      $scope.enable_highlight = true;
-
-      // Add an element
-      $scope.userBlob.data.contacts.unshift(contact);
-
-      // Hide the form
-      $scope.toggle_form();
-
-      // Clear form
       $scope.reset_form();
-    };
-  }]);
+
+      /**
+       * Toggle "add contact" form
+       */
+      $scope.toggle_form = function ()
+      {
+        $scope.addform_visible = !$scope.addform_visible;
+        $scope.reset_form();
+
+        // Focus on first input
+        setImmediate(function() {
+          $('#addForm').find('input:first').focus();
+        });
+      };
+
+      /**
+       * Create contact
+       */
+      $scope.create = function ()
+      {
+        var contact = {
+          name: $scope.contact.name,
+          address: $scope.contact.address,
+          dt: $scope.contact.dt,
+          // Used for animation
+          justAdded: true
+        };
+
+        // Enable the animation
+        $scope.enable_highlight = true;
+
+        // Add an element
+        $scope.userBlob.data.contacts.unshift(contact);
+
+        // Hide the form
+        $scope.toggle_form();
+
+        // Clear form
+        $scope.reset_form();
+      };
+    }]);
 
   module.controller('ContactRowCtrl', ['$scope', '$location',
-                                     function ($scope, $location) {
-    $scope.editing = false;
-
-    /**
-     * Switch to edit mode
-     *
-     * @param index
-     */
-    $scope.edit = function (index)
-    {
-      $scope.editing = true;
-      $scope.editname = $scope.entry.name;
-      $scope.editaddress = $scope.entry.address;
-    };
-
-    /**
-     * Update contact
-     *
-     * @param index
-     */
-    $scope.update = function (index)
-    {
-      if (!$scope.inlineAddress.editaddress.$error.rpUnique
-          && !$scope.inlineAddress.editaddress.$error.rpAddress
-          && !$scope.inlineName.editname.$error.rpUnique) {
-        // Update blob
-        $scope.entry.name = $scope.editname;
-        $scope.entry.address = $scope.editaddress;
-        $scope.editing = false;
-      }
-    };
-
-    /**
-     * Remove contact
-     *
-     * @param index
-     */
-    $scope.remove = function (index) {
-      // Update blob
-      $scope.userBlob.data.contacts.splice(index,1);
-    };
-
-    /**
-     * Cancel contact edit
-     *
-     * @param index
-     */
-    $scope.cancel = function (index)
-    {
+    function ($scope, $location) {
       $scope.editing = false;
-    };
 
-    $scope.send = function (index)
-    {
-      $location.path('/send');
-      $location.search({to: $scope.entry.name});
-    };
-  }]);
+      /**
+       * Switch to edit mode
+       *
+       * @param index
+       */
+      $scope.edit = function (index)
+      {
+        $scope.editing = true;
+        $scope.editname = $scope.entry.name;
+        $scope.editaddress = $scope.entry.address;
+        $scope.editdt = $scope.entry.dt;
+      };
+
+      /**
+       * Update contact
+       *
+       * @param index
+       */
+      $scope.update = function (index)
+      {
+        if (!$scope.inlineAddress.editaddress.$error.rpUnique
+            && !$scope.inlineAddress.editaddress.$error.rpAddress
+            && !$scope.inlineName.editname.$error.rpUnique) {
+          // Update blob
+          $scope.entry.name = $scope.editname;
+          $scope.entry.address = $scope.editaddress;
+          $scope.entry.dt = $scope.editdt;
+          $scope.editing = false;
+        }
+      };
+
+      /**
+       * Remove contact
+       *
+       * @param index
+       */
+      $scope.remove = function (index) {
+        // Update blob
+        $scope.userBlob.data.contacts.splice(index,1);
+      };
+
+      /**
+       * Cancel contact edit
+       *
+       * @param index
+       */
+      $scope.cancel = function (index)
+      {
+        $scope.editing = false;
+      };
+
+      $scope.send = function (index)
+      {
+        $location.path('/send');
+        $location.search({to: $scope.entry.name});
+      };
+    }]);
 };
 
 module.exports = ContactsTab;
