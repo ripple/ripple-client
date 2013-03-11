54dfcbb732449964f9cf432827f03369afb298f4
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-03-11 13:10:58 +0400

    Send tab: st/dt validator. #279

--- src/js/directives/validators.js
+++ src/js/directives/validators.js
@@ -110,6 +110,38 @@ module.directive('rpDestination', function () {
   };
 });
 
+/**
+ * Source and destination tags validator
+ *
+ * Integer in the range 0 to 2^32-1
+ */
+module.directive('rpStdt', function () {
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function (scope, elm, attr, ctrl) {
+      if (!ctrl) return;
+
+      var validator = function(value) {
+        if (!value || (!isNaN(parseFloat(value)) && isFinite(value) && value >= 0 && value < Math.pow(2,32) - 1)) {
+          ctrl.$setValidity('rpStdt', true);
+          return value;
+        } else {
+          ctrl.$setValidity('rpStdt', false);
+          return;
+        }
+      };
+
+      ctrl.$formatters.push(validator);
+      ctrl.$parsers.unshift(validator);
+
+      attr.$observe('rpStdt', function() {
+        validator(ctrl.$viewValue);
+      });
+    }
+  };
+});
+
 module.directive('rpNotMe', function () {
   return {
     restrict: 'A',
