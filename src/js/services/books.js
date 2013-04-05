e9c852d1ecc85a5eb8d6f15e5a7c5ddaf668aea1
Justin Hernandez <justin@ripple.com>
2013-04-05 16:53:23 -0700

    cleaning books.js code

--- src/js/services/books.js
+++ src/js/services/books.js
@@ -8,53 +8,52 @@ var module = angular.module('books', ['network']);
 var Amount = ripple.Amount;
 
 module.factory('rpBooks', ['rpNetwork', '$q', '$rootScope', '$filter',
-                           function (net, $q, $scope, $filter)
-{
+
+function(net, $q, $scope, $filter) {
   function loadBook(gets, pays, taker) {
     return net.remote.book(gets.currency, gets.issuer,
-                           pays.currency, pays.issuer,
-                           taker);
+    pays.currency, pays.issuer,
+    taker);
   }
 
-   function filterRedundantPrices(data, action, combine) {
-      var price;
-      var lastprice;
-      var current;
-      var rpamount = $filter('rpamount');
-      var numerator;
-      var demoninator;
+  function filterRedundantPrices(data, action, combine) {
+    var price;
+    var lastprice;
+    var current;
+    var rpamount = $filter('rpamount');
+    var numerator;
+    var demoninator;
 
-      if (action=='bidsa')
-        return data;
+    data = _.compact(_.map(data, function(d, i) {
+      var numerator = (action == 'asks') ? d.TakerPays : d.TakerGets;
+      var denominator = (action == 'asks') ? d.TakerGets : d.TakerPays;
+      var price = rpamount(Amount.from_json(numerator).ratio_human(denominator), {
+        rel_precision: 4,
+        rel_min_precision: 2
+      });
 
-      data = _.compact(_.map(data, function(d, i){
-        numerator = (action == 'asks') ? d.TakerPays : d.TakerGets;
-        denominator = (action == 'asks') ? d.TakerGets : d.TakerPays;
-        price = rpamount(Amount.from_json(numerator).ratio_human(denominator), {rel_precision: 4, rel_min_precision: 2} );
-
-        if (lastprice == price) {
-          if (combine) {
-            if (action == 'asks') {
-              data[current].TakerPays = Amount.from_json(data[current].TakerPays).add(d.TakerPays).to_number();
-              data[current].TakerGets = Amount.from_json(data[current].TakerGets).add(d.TakerGets).to_json();
-            } else {
-              data[current].TakerPays = Amount.from_json(data[current].TakerPays).add(d.TakerPays).to_json();
-              data[current].TakerGets = Amount.from_json(data[current].TakerGets).add(d.TakerGets).to_number();
-            }
+      if (lastprice == price) {
+        if (combine) {
+          if (action == 'asks') {
+            data[current].TakerPays = Amount.from_json(data[current].TakerPays).add(d.TakerPays).to_number();
+            data[current].TakerGets = Amount.from_json(data[current].TakerGets).add(d.TakerGets).to_json();
+          } else {
+            data[current].TakerPays = Amount.from_json(data[current].TakerPays).add(d.TakerPays).to_json();
+            data[current].TakerGets = Amount.from_json(data[current].TakerGets).add(d.TakerGets).to_number();
           }
-          d = false;
-        } else current = i;
-        lastprice = price;
+        }
+        d = false;
+      } else current = i;
+      lastprice = price;
 
-        return d;
-      }));
+      return d;
+    }));
 
-      return data;
-    }
+    return data;
+  }
 
   return {
-    get: function (first, second, taker)
-    {
+    get: function(first, second, taker) {
       var asks = loadBook(first, second, taker);
       var bids = loadBook(second, first, taker);
 
@@ -67,6 +66,7 @@ module.factory('rpBooks', ['rpNetwork', '$q', '$rootScope', '$filter',
         model.asks = filterRedundantPrices(offers, 'asks', true);
         $scope.$digest();
       }
+
       function handleAskTrade(gets, pays) {
         model.last_price = gets.ratio_human(pays);
         $scope.$digest();
@@ -78,6 +78,7 @@ module.factory('rpBooks', ['rpNetwork', '$q', '$rootScope', '$filter',
         model.bids = filterRedundantPrices(offers, 'bids', true);
         $scope.$digest();
       }
+
       function handleBidTrade(gets, pays) {
         model.last_price = pays.ratio_human(gets);
         $scope.$digest();
@@ -85,7 +86,7 @@ module.factory('rpBooks', ['rpNetwork', '$q', '$rootScope', '$filter',
       bids.on('model', handleBidModel);
       bids.on('trade', handleBidTrade);
 
-      model.unsubscribe = function () {
+      model.unsubscribe = function() {
         asks.removeListener('model', handleAskModel);
         asks.removeListener('trade', handleAskTrade);
         bids.removeListener('model', handleBidModel);
