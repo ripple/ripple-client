44b9da339ce9f47378c6504227b52cfdfd97b6fb
wltsmrz <chlavois@gmail.com>
2013-05-17 06:34:17 +0900

    Trade tab: Update URL, allow linking to direct order books. Closes #397

--- src/js/entry/desktop.js
+++ src/js/entry/desktop.js
@@ -67,7 +67,15 @@ app.config(['$routeProvider', '$injector', function ($routeProvider, $injector) 
           mainMenu: tab.mainMenu,
           template: tab.generateHtml()
         };
+
         $routeProvider.when('/'+tabName, config);
+
+        if (tab.extraRoutes) {
+          _.each(tab.extraRoutes, function(route) {
+            $.extend({}, config, route.config);
+            $routeProvider.when(route.name, config);
+          });
+        }
 
         _.each(tab.aliases, function (alias) {
           $routeProvider.when('/'+alias, config);
@@ -95,11 +103,6 @@ app.run(['$rootScope', '$injector', '$compile', '$route', '$routeParams', '$loca
   // if url has a + or %2b then replace with %20 and redirect
   if (_.isArray($location.$$absUrl.match(/%2B|\+/gi)))
     window.location = $location.$$absUrl.replace(/%2B|\+/gi, '%20');
-
-  // Close off-canvas menu on page change
-  $rootScope.$on('$routeChangeStart', function () {
-    $('.off-canvas-nav-expand').removeClass('off-canvas-nav-expand');
-  });
 
   var scope = $rootScope;
   $rootScope.$route = $route;
