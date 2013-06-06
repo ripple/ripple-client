0aac8c46b78e7663567ccfa4222de01730011b1d
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-06-06 19:06:17 +0400

    rpPositiveAmount fixes

--- src/js/directives/validators.js
+++ src/js/directives/validators.js
@@ -498,7 +498,7 @@ module.directive('rpPositiveAmount', function () {
 
         ctrl.$setValidity('rpAmount', amount.is_valid() && amount.to_json() > 0);
 
-        return amount.to_json();
+        return value < 1 ? amount.to_json() / 1000000 : amount.to_json();
       };
 
       ctrl.$formatters.push(validator);
