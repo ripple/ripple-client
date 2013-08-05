36bc9cb4e30960a9bea9471274e3741105a0c8f5
Nicholas Dudfield <ndudfield@gmail.com>
2013-08-05 12:18:45 -0700

    Updated the karma confs

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -1,19 +1,28 @@
-basePath = '../';
+basePath =  '../';
 
 files = [
   ANGULAR_SCENARIO,
   ANGULAR_SCENARIO_ADAPTER,
-  {pattern: 'index_debug.html', included: false},
-  {pattern: 'build/css/*.css', included: false},
-  {pattern: 'build/dist/*.js', included: false},
-  {pattern: '*.js', included: false, watch: false},
-  {pattern: 'fonts/*', included: false, watch: false},
-  {pattern: 'deps/js/modernizr-2.5.3.min.js', included: false, watch: false},
-  {pattern: 'img/*', included: false, watch: false},
+  // {pattern: 'index_debug.html', included: false},
+  // {pattern: 'build/css/*.css', included: false},
+  // {pattern: 'build/dist/*.js', included: false},
+  // {pattern: '*.js', included: false, watch: false},
+  // // {pattern: 'test/e2e/scenarios.js', included: false, watch: true},
+
+  // {pattern: 'fonts/*', included: false, watch: false},
+  // {pattern: 'deps/js/modernizr-2.5.3.min.js', included: false, watch: false},
+  // {pattern: 'img/*', included: false, watch: false},
   'test/e2e/**/*.js'
 ];
 
-autoWatch = false;
+singleRun = false;
+autoWatch = true;
+
+proxies = {
+  '/': 'http://localhost:8000/'
+};
+
+urlRoot = '/tests';
 
 browsers = ['Chrome'];
 
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,20 +1,41 @@
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    basePath: '../',
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'build/dist/deps-debug.js',
-  'test/lib/angular/angular-mocks.js',
-  'config.js',
-  'build/dist/ripple-client-desktop-debug.js',
-  'test/unit/**/*.js'
-];
+    files: [
+      'build/dist/deps-debug.js',
+      'test/lib/angular/angular-mocks.js',
+      'config.js',
+      'build/dist/ripple-client-desktop-debug.js',
+      'test/unit/**/*.js'
+    ],
 
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+    browsers: ['Chrome'],
+    singleRun: false,
+    autoWatch: true
+  });
 };
+
+
+// basePath = '../';
+
+// files = [
+//   JASMINE,
+//   JASMINE_ADAPTER,
+//   'build/dist/deps-debug.js',
+//   'test/lib/angular/angular-mocks.js',
+//   'config.js',
+//   'build/dist/ripple-client-desktop-debug.js',
+//   'test/unit/**/*.js'
+// ];
+
+// autoWatch = true;
+// singleRun = false;
+
+// browsers = ['Chrome'];
+
+// junitReporter = {
+//   outputFile: 'test_out/unit.xml',
+//   suite: 'unit'
+// };
