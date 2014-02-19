61d76d00fb219a3cfa119b78d4dc63574da78f47
Vahe Hovhannisyan <vhpoet@gmail.com>
2014-02-19 14:09:14 -0800

    Add new, hidden Apps tab.

---
+++ src/js/tabs/apps.js
@@ -0,0 +1,68 @@
+var util = require('util'),
+    webutil = require('../util/web'),
+    Tab = require('../client/tab').Tab,
+    Amount = ripple.Amount,
+    Base = ripple.Base;
+
+var AppsTab = function ()
+{
+  Tab.call(this);
+};
+
+util.inherits(AppsTab, Tab);
+
+AppsTab.prototype.tabName = 'apps';
+AppsTab.prototype.mainMenu = 'apps';
+
+AppsTab.prototype.generateHtml = function ()
+{
+  return require('../../jade/tabs/apps.jade')();
+};
+
+AppsTab.prototype.angular = function (module)
+{
+  module.controller('AppsCtrl', ['$scope', '$timeout', '$routeParams', 'rpId', 'rpAppManager',
+    function ($scope, $timeout, $routeParams, $id, $appManager)
+  {
+    if (!$id.loginStatus) return $id.goId();
+
+    /**
+     * Add an app
+     */
+    $scope.add = function(){
+      $scope.loading = true;
+
+      if ($scope.app && $scope.app.rippleAddress) {
+        $appManager.getApp($scope.app.rippleAddress, function(err, app){
+          if (err) {
+            console.log('err',err);
+            $scope.error = err.message;
+            return;
+          }
+
+          $scope.app.name = app.name;
+          $scope.app.type = app.type;
+
+          $scope.userBlob.unshift("/apps", $scope.app);
+          $scope.success = true;
+
+          $scope.loading = false;
+        });
+      }
+    };
+
+    /**
+     * Remove app
+     *
+     * @param index
+     */
+    $scope.remove = function (rippleAddress) {
+      // TODO this should also close/remove account, disconnect from an app.
+
+      // Update blob
+      $scope.userBlob.filter('/apps', 'rippleAddress', rippleAddress, 'unset', '');
+    };
+  }]);
+};
+
+module.exports = AppsTab;
