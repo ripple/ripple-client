1482a7e8b004d9ee55d81222266657999736c2a5
Vahe Hovhannisyan <vhpoet@gmail.com>
2014-02-19 16:51:58 -0800

    Add super user hidden tab.

---
+++ src/js/tabs/su.js
@@ -0,0 +1,66 @@
+var util = require('util'),
+    webutil = require('../util/web'),
+    Tab = require('../client/tab').Tab,
+    Amount = ripple.Amount,
+    Base = ripple.Base;
+
+var SuTab = function ()
+{
+  Tab.call(this);
+};
+
+util.inherits(SuTab, Tab);
+
+SuTab.prototype.tabName = 'su';
+SuTab.prototype.mainMenu = 'su';
+
+SuTab.prototype.generateHtml = function ()
+{
+  return require('../../jade/tabs/su.jade')();
+};
+
+SuTab.prototype.angular = function (module)
+{
+  module.controller('SuCtrl', ['$scope', '$timeout', '$routeParams', 'rpId',
+                               'rpNetwork', 'rpDomainAlias', 'rpKeychain',
+    function ($scope, $timeout, $routeParams, id, net, aliasService, keychain)
+  {
+    if (!id.loginStatus) return id.goId();
+
+    $scope.account = {};
+
+    // Get account
+    $scope.$on('$idAccountLoad', function (e, data) {
+      var alias = aliasService.getAliasForAddress(data.account);
+
+      alias.then(
+        function(domain){
+          $scope.account.domain = domain;
+        },
+        function(reason){
+          console.log('error', reason);
+        }
+      );
+    });
+
+    $scope.accountSet = function() {
+      var tx = net.remote.transaction();
+
+      tx.accountSet(id.account);
+      tx.tx_json.Domain = sjcl.codec.hex.fromBits(sjcl.codec.utf8String.toBits($scope.account.domain));
+      tx.on('success', function(){
+        console.log('Cool!');
+      });
+
+      keychain.requestSecret(id.account, id.username, function (err, secret) {
+        // TODO Error handling
+        if (err) return;
+
+        tx.secret(secret);
+        tx.submit();
+      });
+    };
+  }]);
+};
+
+module.exports = SuTab;
