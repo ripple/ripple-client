a359a279bf39f7e4de181bc86da8e4124d4eb97f
Stefan Thomas <justmoon@members.fsf.org>
2014-04-10 22:45:56 -0700

    [FEATURE] Use new demurrage info directive in rp-pretty-amount.

--- src/js/directives/formatters.js
+++ src/js/directives/formatters.js
@@ -61,7 +61,7 @@ module.directive('rpPrettyAmount', [function () {
       amount: '=rpPrettyAmount'
     },
     template: '<span class="value">{{amount | rpamount:{reference_date:date} }}</span> ' +
-              '<span class="currency">{{amount | rpcurrency}}</span>',
+              '<span class="currency" rp-currency="amount"></span>',
     compile: function (element, attr, linker) {
       return function (scope, element, attr) {
         scope.date = scope.date || element.inheritedData(RP_PRETTY_AMOUNT_DATE);
