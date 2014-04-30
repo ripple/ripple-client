98aa77e0464260af01361026a0771ab5b2564995
Vahe Hovhannisyan <vhpoet@gmail.com>
2014-04-30 15:08:29 -0700

    rpSpinner: Spinner should support input fields.

--- src/js/directives/directives.js
+++ src/js/directives/directives.js
@@ -412,14 +412,25 @@ module.directive('rpSpinner', [function() {
         }
 
         if (value > 0) {
-          element.addClass('spinner');
           spinner = new Spinner({
             lines: 9, // The number of lines to draw
             length: 3, // The length of each line
             width: 2, // The line thickness
             radius: value, // The radius of the inner circle
             className: 'spinnerInner'
-          }).spin(element[0]);
+          });
+
+          // Spinner for input field
+          if (element.is('input')) {
+            element.after('<div class="inputSpinner"></div>');
+            spinner.spin(element.parent().find('.inputSpinner')[0]);
+          }
+
+          // Spinner for everything else
+          else {
+            element.addClass('spinner');
+            spinner.spin(element[0]);
+          }
         }
       });
     }
