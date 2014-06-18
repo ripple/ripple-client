c2a5a63bfc60c9b711e0c645fc693e9f7a3da453
Anna Tong <anna.l.tong@gmail.com>
2014-06-17 17:40:02 -0700

    [CHORE] Currency pairs in trade tab: update order
    Update the order of the currency pairs in the Trade tab dropdown menu, requested by Phil. 
    Reflects "the majors" and XRP vs "the majors," plus selected other currencies.

--- src/js/data/pairs.js
+++ src/js/data/pairs.js
@@ -8,21 +8,23 @@
  */
 module.exports = [
   {name: 'BTC/XRP', order: 1},
-  {name: 'XRP/USD', order: 1},
+  {name: 'BTC/EUR', order: 1},
+  {name: 'BTC/USD', order: 1},
   {name: 'XRP/EUR', order: 1},
-  {name: 'XRP/JPY', order: 0},
   {name: 'XRP/GBP', order: 0},
   {name: 'XRP/AUD', order: 0},
-  {name: 'XRP/CHF', order: 0},
+  {name: 'XRP/USD', order: 0},
   {name: 'XRP/CAD', order: 0},
-  {name: 'XRP/CNY', order: 0},
+  {name: 'XRP/CHF', order: 0},
+  {name: 'XRP/BRL', order: 0},
   {name: 'XRP/MXN', order: 0},
-  {name: 'BTC/USD', order: 0},
-  {name: 'BTC/EUR', order: 0},
+  {name: 'XRP/JPY', order: 0},
+  {name: 'XRP/CNY', order: 0},
   {name: 'EUR/USD', order: 0},
-  {name: 'USD/JPY', order: 0},
   {name: 'GBP/USD', order: 0},
   {name: 'AUD/USD', order: 0},
-  {name: 'USD/MXN', order: 0},
-  {name: 'USD/CHF', order: 0}
+  {name: 'USD/CAD', order: 0}.
+  {name: 'USD/CHF', order: 0}.
+  {name: 'USD/JPY', order: 0},
+  {name: 'USD/CNY', order: 0},
 ];
