7ed1a104e2578c0f0f1d6fb18fec279d432e8c1e
Yong-Soo Chung <yongsoochung@gmail.com>
2014-06-18 08:34:51 -0700

    [CHORE] Trustlines: Fix visual UI of trust line deletion, fix bug (WC-1620)

--- src/js/tabs/trust.js
+++ src/js/tabs/trust.js
@@ -428,7 +428,7 @@ TrustTab.prototype.angular = function (module)
           return tx;
         }
 
-        var tx = $scope.orderbookExists ? sendBalanceToSelf() : sendBalanceToIssuer();
+        var tx = ($scope.orderbookStatus === 'exists') ? sendBalanceToSelf() : sendBalanceToIssuer();
 
         setSecretAndSubmit(tx);
 
@@ -447,12 +447,6 @@ TrustTab.prototype.angular = function (module)
 
       else {
         nullifyTrustLine(id.account, $scope.currency, $scope.counterparty);
-      }
-
-      for (var address in $scope.lines) {
-        if (address === ($scope.counterparty + $scope.currency)) {
-          delete $scope.lines[address];
-        }
       }
 
       $scope.toggle_form();
