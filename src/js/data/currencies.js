bf8f6e64680856adcfb512033d41ddf88a586e1a
Anna Tong <anna.l.tong@gmail.com>
2014-06-19 17:29:15 -0700

    [FIX] Currencies dropdown ordering
    Fix the dropdown currencies ordering to reflect most commonly traded currencies & suggested default rankings

--- src/js/data/currencies.js
+++ src/js/data/currencies.js
@@ -2,33 +2,34 @@
  * Ripple default external currency list.
  *
  * These currencies are ranked by value of notes in circulation. Source:
- *
+ * most popular sent currencies on  Ripple Trade, default currency rankings, and
  * * http://goldnews.bullionvault.com/all_the_money_in_the_world_102720093
- *   (A better source is welcome. Note: The US dollar was moved to the top.)
  *
  * Important: XRP must be the first entry in this list.
  */
 module.exports = [
-  {value: 'XRP', name: 'Ripples', order: 5},
-  {value: 'USD', name: 'US Dollar', order: 4},
-  {value: 'EUR', name: 'Euro', order: 3},
-  {value: 'BTC', name: 'Bitcoin', order: 2},
-  {value: 'LTC', name: 'Litecoin', order: 1},
-  {value: 'JPY', name: 'Japanese Yen', order: 0},
-  {value: 'CNY', name: 'Chinese Yuan', order: 0},
+  {value: 'XRP', name: 'Ripples', order: 12},
+  {value: 'BTC', name: 'Bitcoin', order: 11},
+  {value: 'LTC', name: 'Litecoin', order: 10},
+  {value: 'JPY', name: 'Japanese Yen', order: 9},
+  {value: 'CNY', name: 'Chinese Yuan', order: 8}, 
+  {value: 'EUR', name: 'Euro', order: 7},
+  {value: 'USD', name: 'US Dollar', order: 6},
+  {value: 'GBP', name: 'British Pound', order: 5},
+  {value: 'AUD', name: 'Australian Dollar', order: 4},
+  {value: 'NZD', name: 'New Zealand Dollar', order: 3},
+  {value: 'CAD', name: 'Canadian Dollar', order: 2},
+  {value: 'CHF', name: 'Swiss Franc', order: 1},
+  {value: 'SGD', name: 'Singapore Dollar', order: 0},   
+  {value: 'NOK', name: 'Norwegian Krone', order: 0},  
+  {value: 'BRL', name: 'Brazilian Real', order: 0},
+  {value: 'MXN', name: 'Mexican Peso', order: 0},
   {value: 'INR', name: 'Indian Rupee', order: 0},
   {value: 'RUB', name: 'Russian Ruble', order: 0},
-  {value: 'GBP', name: 'British Pound', order: 0},
-  {value: 'CAD', name: 'Canadian Dollar', order: 0},
-  {value: 'BRL', name: 'Brazilian Real', order: 0},
-  {value: 'CHF', name: 'Swiss Franc', order: 0},
   {value: 'DKK', name: 'Danish Krone', order: 0},
-  {value: 'NOK', name: 'Norwegian Krone', order: 0},
   {value: 'SEK', name: 'Swedish Krona', order: 0},
   {value: 'CZK', name: 'Czech Koruna', order: 0},
   {value: 'PLN', name: 'Polish Zloty', order: 0},
-  {value: 'AUD', name: 'Australian Dollar', order: 0},
-  {value: 'MXN', name: 'Mexican Peso', order: 0},
   {value: 'KRW', name: 'South Korean Won', order: 0},
   {value: 'TWD', name: 'New Taiwan Dollar', order: 0},
   {value: 'HKD', name: 'Hong Kong Dollar', order: 0},
@@ -36,7 +37,6 @@ module.exports = [
   {value: 'AMD', name: 'Armenian Drams', order: 0},
   {value: 'RUR', name: 'Russian Rubles', order: 0},
   {value: 'RON', name: 'Romanian Leu', order: 0},
-  {value: 'NZD', name: 'New Zealand Dollar', order: 0},
   {value: 'TRY', name: 'Turkish Lira', order: 0},
   {value: 'XAU', name: 'Gold', order: 0},
   {value: 'XAG', name: 'Silver', order: 0},
