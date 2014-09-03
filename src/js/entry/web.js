917d90c7956a1f231579a9a635b321a3521c5c26
Malika Rajvanshy <malika.rajvanshy@gmail.com>
2014-09-03 15:58:15 -0700

    [FEATURE] Initial deposit USD flow

--- src/js/entry/web.js
+++ src/js/entry/web.js
@@ -88,7 +88,6 @@ var tabdefs = [
   require('../tabs/withdraw'),
   require('../tabs/usd'),
   require('../tabs/eula'),
-  require('../tabs/deposit'),
 
   // Hidden tabs
   require('../tabs/apps'),
