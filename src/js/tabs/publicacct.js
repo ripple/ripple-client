1b93c34d03949911cf142d0cec18d3976fb5ed4a
Yong-Soo Chung <yongsoochung@gmail.com>
2014-09-08 18:43:57 -0700

    [TASK] Accts: Restructure accounts page for public/private info

---
+++ src/js/tabs/publicacct.js
@@ -0,0 +1,101 @@
+var util = require('util');
+var Tab = require('../client/tab').Tab;
+
+var PublicAcctTab = function ()
+{
+  Tab.call(this);
+};
+
+util.inherits(PublicAcctTab, Tab);
+
+PublicAcctTab.prototype.tabName = 'publicacct';
+PublicAcctTab.prototype.mainMenu = 'publicacct';
+
+PublicAcctTab.prototype.generateHtml = function ()
+{
+  return require('../../jade/tabs/publicacct.jade')();
+};
+
+PublicAcctTab.prototype.angular = function(module)
+{
+  module.controller('PublicAcctCtrl', ['$scope', '$timeout', 'rpId', 'rpKeychain',
+    function ($scope, $timeout, $id, keychain)
+    {
+      if (!$id.loginStatus) return $id.goId();
+
+      $scope.infoPage = 'public';
+
+
+      $scope.rename = function() {
+        $scope.loading = true;
+        $scope.error = false;
+
+        // Get the master key
+        keychain.getSecret($id.account, $id.username, $scope.password,
+          function (err, masterkey) {
+            if (err) {
+              console.log("client: account tab: error while " +
+                "unlocking wallet: ", err);
+
+              $scope.error = 'wrongpassword';
+              $scope.loading = false;
+              return;
+            }
+
+            // Rename
+            $id.rename({
+              new_username: $scope.username,
+              password: $scope.password,
+              masterkey: masterkey
+            }, function(err){
+              if (err) {
+                console.log('client: account tab: error while ' +
+                  'renaming account: ', err);
+                $scope.error = true;
+                $scope.loading = false;
+                return;
+              }
+
+              // Re-login
+              // TODO implement refresh/relogin in ID.
+              $id.login({
+                username: $scope.username,
+                password: $scope.password
+              }, function (err) {
+                if (err) {
+                  console.log('client: account tab: error while ' +
+                    'logging user in: ', err);
+                  $scope.error = 'cantlogin';
+                  $scope.loading = false;
+                  return;
+                }
+
+                $scope.success = true;
+                reset();
+              });
+            });
+          }
+        );
+      };
+
+      var reset = function() {
+        $scope.openForm = false;
+        $scope.username = '';
+        $scope.password = '';
+        $scope.showPassword = true;
+        $scope.loading = false;
+        $scope.error = false;
+
+        if ($scope.renameForm) {
+          $scope.renameForm.$setPristine(true);
+        }
+
+      };
+
+      reset();
+      $scope.success = false;
+    }]
+  );
+};
+
+module.exports = PublicAcctTab;
