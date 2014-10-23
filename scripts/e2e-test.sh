b539c5782728d5d550379515d690f06bc828e53e
Norman Barber <normanebarber@gmail.com>
2014-10-22 22:34:56 -0400

    [TASK] auto bat and shell scripts to run tests

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 BASE_DIR=`dirname $0`
-
 echo ""
 echo "Starting e2e test suite"
-echo "-------------------------------------------------------------------"
 
-$BASE_DIR/../node_modules/protractor/bin/protractor $BASE_DIR/../test/config/protractor.conf.js %*
-
+$BASE_DIR/../node_modules/protractor/bin/protractor $BASE_DIR/../test/protractor.conf.js %*
 echo "e2e test suite complete"
