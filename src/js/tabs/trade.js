9aa31dbb5eb1394d19c85ae24cf7d87381928283
Vahe Hovhannisyan <vhpoet@gmail.com>
2014-12-02 10:43:29 -0800

    [FIX] Trade: Coding style

--- src/js/tabs/trade.js
+++ src/js/tabs/trade.js
@@ -410,15 +410,16 @@ TradeTab.prototype.angular = function(module)
       order.first_issuer = this.entry.first.issuer().to_json();
       order.second_currency = this.entry.second.currency().to_json();
       order.second_issuer = this.entry.second.issuer().to_json();
-      if(this.entry.first.currency().to_json() == 'XRP'){
-        order.currency_pair = this.entry.first.currency().to_json() + '/' + this.entry.second.currency().to_json() + '.' + this.entry.second.issuer().to_json();
-      }
-      else if (this.entry.second.currency().to_json() == 'XRP'){
-        order.currency_pair = this.entry.first.currency().to_json() + '.' + this.entry.first.issuer().to_json() + '/' + this.entry.second.currency().to_json();
-      }
-      else {
-        order.currency_pair = this.entry.first.currency().to_json() + '.' + this.entry.first.issuer().to_json() + '/' + this.entry.second.currency().to_json() + '.' + this.entry.second.issuer().to_json();
-      }
+
+      var first = order.first_currency == 'XRP'
+        ? 'XRP'
+        : order.first_currency + '.' + order.first_issuer;
+
+      var second = order.second_currency == 'XRP'
+        ? 'XRP'
+        : order.second_currency + '.' + order.second_issuer;
+
+      order.currency_pair = first + '/' + second;
 
       var changedPair = updateSettings();
       //updateMRU();
