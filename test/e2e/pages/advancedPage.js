31fca496b714d4c76ad5ed60369d857847d56d64
Ivan Tivonenko <darkdarkdragon@gmail.com>
2014-12-02 04:48:52 +0200

    [TEST] fix gateway test
    make it work with changes on advanced.jade

--- test/e2e/pages/advancedPage.js
+++ test/e2e/pages/advancedPage.js
@@ -4,8 +4,8 @@ var AdvancedPage = function () {
   this.editAdvancedSettingsButton = $('form[name="accountsAdvForm"] div[ng-hide="editAcctOptions"] a.btn-cancel');
   this.showAdvancedOptionsInput = $('input[name="acct_adv"]');
   this.saveAdvancedSettingsButton = $('form[name="accountsAdvForm"] #save');
-  this.advancedSettingsShowHolder = $('form[name="accountsAdvForm"] div[ng-show="advanced_feature_switch"]');
-  this.advancedSettingsHideHolder = $('form[name="accountsAdvForm"] div[ng-hide="advanced_feature_switch"]');
+  this.advancedSettingsShowHolder = $('form[name="accountsAdvForm"] div[ng-show="options.advanced_feature_switch"]');
+  this.advancedSettingsHideHolder = $('form[name="accountsAdvForm"] div[ng-hide="options.advanced_feature_switch"]');
 };
 
 AdvancedPage.prototype.goTo = function () {
