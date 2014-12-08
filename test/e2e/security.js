6a81affe90f201525a7de733fd603f051207070b
rht <rudyht@gmail.com>
2014-12-08 19:29:52 +0800

    [TEST] Security: Fix change password test
    Also, now refactored with page object.
    Let me know if it doesn't work.

--- test/e2e/security.js
+++ test/e2e/security.js
@@ -12,71 +12,83 @@ var expect = chai.expect;
 
 describe('password setting', function() {
 
-    function changePassword(oldpassword, newpassword) {
+    // security page object
+    var SecurityPage = function() {
+      this.changePasswordButton = $('a[ng-hide="openFormPassword"][l10n]');
+      this.passwordForm = $('#renameForm');
+      this.passwordNotification = $('.alert.alert-success[ng-show=success.changePassword]');
+
+      this.get = function() {
+        browser.get('#/security');
+        browser.wait($('.showSecurity').isDisplayed);
+      };
+
+      this.changePassword = function(oldpassword, newpassword) {
         $('input[name=password]').sendKeys(oldpassword);
         $('input[name=change_password1]').sendKeys(newpassword);
         $('input[name=change_password2]').sendKeys(newpassword);
         return $('#renameForm button[type=submit]').click();
-    }
+      };
+    };
+    var securityPage = new SecurityPage();
 
     before(function(){
-        helperForms.login(config.user.username, config.user.password);
+      helperForms.login(config.user.username, config.user.password);
     });
 
     it('should render the security page', function(done) {
-        // Go to send page
-        browser.get('#/security');
+      // Go to send page
+      securityPage.get();
 
-        // Wait for the security page to render
-        browser.wait($('.showSecurity').isDisplayed);
-        expect($('a[ng-hide=openFormPassword]').isDisplayed())
-          .to.eventually.be.true
-          .and.notify(done);
+      // Wait for the security page to render
+      expect(securityPage.changePasswordButton.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
 
     it('should render the password form', function(done) {
-        $('a[ng-hide=openFormPassword]').click();
-        expect($('#renameForm').isDisplayed())
-          .to.eventually.be.true
-          .and.notify(done);
+      securityPage.changePasswordButton.click();
+      expect(securityPage.passwordForm.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
 
     var tempPassword = config.user.password + '-e2e';
-    //browser.ignoreSynchronization = true;
+    protractor.getInstance().ignoreSynchronization = true;
 
     it('should change the password', function(done) {
-        changePassword(config.user.password, tempPassword);
-        expect($('[ng-show=success].alert.alert-success').isDisplayed())
-          .to.eventually.be.true
-          .and.notify(done);
+      securityPage.changePassword(config.user.password, tempPassword);
+      expect(securityPage.passwordNotification.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
 
     it('should login with the new password', function(done) {
-        helperForms.login(config.user.username, tempPassword);
+      helperForms.login(config.user.username, tempPassword);
     });
 
     it('should render the security page again', function(done) {
-        // sleep for 2s
-        browser.sleep(2000);
-        // Wait for the security page to render
-        browser.get('#/security');
-        browser.wait($('.showSecurity').isDisplayed);
-        expect($('a[ng-hide=openFormPassword]').isDisplayed())
-          .to.eventually.be.true
-          .and.notify(done);
+      // sleep for 2s
+      browser.sleep(2000);
+      // Wait for the security page to render
+      securityPage.get();
+      expect(securityPage.changePasswordButton.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
 
     it('should render the password form', function(done) {
-        $('a[ng-hide=openFormPassword]').click();
-        expect($('#renameForm').isDisplayed()).to.eventually.be.true.and.notify(done);
+      securityPage.changePasswordButton.click();
+      expect(securityPage.passwordForm.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
 
     it('should change the password back', function(done) {
 
-        changePassword(tempPassword, config.user.password);
-        expect($('[ng-show=success].alert.alert-success').isDisplayed())
-          .to.eventually.be.true
-          .and.notify(done);
+      securityPage.changePassword(tempPassword, config.user.password);
+      expect(securityPage.passwordNotification.isDisplayed())
+        .to.eventually.be.true
+        .and.notify(done);
     });
-
 });
