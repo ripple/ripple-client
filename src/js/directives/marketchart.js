cf787fb1f33d2b8a72f71ff9522b485535624615
J.M <jm@ripple.com>
2014-12-12 21:08:43 +0100

    [FIX] Market chart: Add more spacing on the right
    This is for BTC, which has tiny numbers and would get cut off.

--- src/js/directives/marketchart.js
+++ src/js/directives/marketchart.js
@@ -24,9 +24,9 @@ module.directive('rpMarketChart', [function() {
 
       chart = PriceChartWidget({
         id:     id,
-        width:  $elem.width() - 115,
+        width:  $elem.width() - 120,
         height: $elem.height() - 20,
-        margin: {top: 0, right: 65, bottom: 20, left: 50},
+        margin: {top: 0, right: 70, bottom: 20, left: 50},
         resize: true
       });
 
