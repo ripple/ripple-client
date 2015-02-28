ce776a861d6ff50631d1a94440547acf4b4d1dbd
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-02-28 06:15:13 +0200

    [TEST] force run sauce tests

--- test/travis-run.sh
+++ test/travis-run.sh
@@ -2,6 +2,7 @@
 set -ev
 grunt
 npm run test-travis
-if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ]; then
+#if [ "${TRAVIS_SECURE_ENV_VARS}" = "true" ]; then
   se-interpreter test/selenium/interpreter_config.json
-fi
+#fi
+
