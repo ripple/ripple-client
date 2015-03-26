ce6eceda2c95b3f53c5681814c9dd475b69d0fff
Mesrop Minasyan <mesrop.minasyan@gmail.com>
2015-03-26 14:38:58 +0400

    [TASK] Reorganize the Settings page (RT-3299)

---
+++ src/js/tabs/settingsgateway.controller.js
@@ -0,0 +1,136 @@
+var util = require('util'),
+    webutil = require('../util/web'),
+    Tab = require('../client/tab').Tab,
+    settings = require('../util/settings'),
+    Currency = ripple.Currency;
+
+var GatewaysTab = function ()
+{
+  Tab.call(this);
+};
+
+util.inherits(GatewaysTab, Tab);
+
+GatewaysTab.prototype.tabName = 'settingsgateway';
+GatewaysTab.prototype.mainMenu = 'settingsgateway';
+
+GatewaysTab.prototype.generateHtml = function ()
+{
+  return require('../../jade/tabs/settingsgateway.jade')();
+};
+
+GatewaysTab.prototype.angular = function(module)
+{
+  module.controller('SettingsGatewayCtrl', ['$scope', 'rpId', 'rpKeychain', 'rpNetwork',
+                                    function ($scope, id, keychain, network)
+  {
+    var xrpCurrency = Currency.from_json('XRP');
+
+    $scope.xrp = {
+      name: xrpCurrency.to_human({full_name:$scope.currencies_all_keyed.XRP.name}),
+      code: xrpCurrency.get_iso(),
+      currency: xrpCurrency
+    };
+
+    $scope.options = Options;
+    $scope.optionsBackup = $.extend(true, {}, Options);
+    $scope.passwordProtection = !settings.getSetting($scope.userBlob, 'persistUnlock', false);
+    $scope.editConfirmation = {
+      send: false,
+      exchange: false,
+      trade: false
+    };
+    $scope.edit = {
+      advanced_feature_switch: false,
+      bridge: false,
+      defaultRippleFlag: false
+    };
+    $scope.max_tx_network_fee_human = ripple.Amount.from_json($scope.options.max_tx_network_fee).to_human();
+    $scope.confirmationChanged = {
+      send: false,
+      exchange: false,
+      trade: false
+    };
+
+    // Initialize the notification object
+    $scope.success = {};
+
+    $scope.save = function(type) {
+      switch (type) {
+        case 'advanced_feature_switch':
+          $scope.userBlob.set('/clients/rippletradecom/trust/advancedMode', $scope.options.advanced_feature_switch);
+          break;
+        case 'defaultRippleFlag':
+          // Need to set flag on account_root
+          var tx = network.remote.transaction();
+          tx.accountSet(id.account);
+          tx.setFlags('DefaultRipple');
+
+          keychain.requestSecret(id.account, id.username, function (err, secret) {
+            if (err) {
+              console.log('Error: ', err);
+              return;
+            }
+            tx.secret(secret);
+            tx.submit();
+          });
+          break;
+        default:
+          // Save in local storage
+          if (!store.disabled) {
+            store.set('ripple_settings', JSON.stringify($scope.options));
+          }
+      }
+
+      $scope.edit[type] = false;
+
+
+      // Notify the user
+      $scope.success[type] = true;
+    };
+
+    $scope.saveConfirmation = function(transactionType) {
+      // ignore it if we are not going to change anything
+      if (!$scope.confirmationChanged[transactionType]) {
+        $scope.editConfirmation[transactionType] = false;
+        return;
+      }
+      $scope.confirmationChanged[transactionType] = false;
+
+      $scope.userBlob.set('/clients/rippletradecom/confirmation', $scope.options.confirmation);
+
+      $scope.editConfirmation[transactionType] = false;
+
+      // Notify the user
+      $scope.success.saveConfirmation[transactionType] = true;
+    };
+
+    $scope.deleteUrl = function(type) {
+      if (type === 'bridge') {
+        $scope.options.bridge.out.bitcoin = '';
+      }
+
+      // Save in local storage
+      if (!store.disabled) {
+        store.set('ripple_settings', JSON.stringify($scope.options));
+      }
+    };
+
+    $scope.cancelEdit = function(type) {
+      $scope.edit[type] = false;
+      if (type === 'bridge') {
+        $scope.options.bridge.out.bitcoin = $scope.optionsBackup.bridge.out.bitcoin;
+      } else {
+        $scope.options[type] = $scope.optionsBackup[type];
+      }
+    };
+
+    $scope.cancelEditConfirmation = function(transactionType) {
+      $scope.editConfirmation[transactionType] = false;
+      $scope.options.confirmation[transactionType] = $scope.optionsBackup.confirmation[transactionType];
+    };
+
+  }]);
+};
+
+module.exports = GatewaysTab;
