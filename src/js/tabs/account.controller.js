de4f628d15752a973eea043c3889b8f7653c397d
Hovhannes Kuloghlyan <chokoc@gmail.com>
2015-06-05 20:58:53 +0400

    [FEATURE] Security: Disable ability to edit ripple name in RT (IID-102)

--- src/js/tabs/account.controller.js
+++ src/js/tabs/account.controller.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var util = require('util');
 var Tab = require('../client/tab').Tab;
 
-var AccountTab = function ()
-{
+var AccountTab = function() {
   Tab.call(this);
 };
 
@@ -12,89 +13,14 @@ AccountTab.prototype.tabName = 'account';
 AccountTab.prototype.mainMenu = 'account';
 
 AccountTab.prototype.extraRoutes = [
-  { name: '/account/:route' }
+  {name: '/account/:route'}
 ];
 
-AccountTab.prototype.angular = function(module)
-{
-  module.controller('AccountCtrl', ['$scope', '$timeout', 'rpId', 'rpKeychain', '$routeParams',
-    function ($scope, $timeout, id, keychain, $routeParams)
-    {
-      if (!$routeParams.route) $routeParams.route = 'public';
-
-      $scope.rename = function() {
-        $scope.loading = true;
-        $scope.error = false;
-        // Get the master key
-        keychain.getSecret(id.account, id.username, $scope.password,
-          function (err, masterkey) {
-            if (err) {
-              console.log('client: account tab: error while ' +
-                'unlocking wallet: ', err);
-
-              if (err instanceof Error && typeof err.message === 'string' && err.message.indexOf('PAKDF') !== -1) {
-                $scope.error = 'server';
-              } else {
-                $scope.error = 'wrongpassword';
-              }
-
-              $scope.loading = false;
-              return;
-            }
-
-            // Rename
-            id.rename({
-              new_username: $scope.username,
-              password: $scope.password,
-              masterkey: masterkey
-            }, function(err){
-              if (err) {
-                console.log('client: account tab: error while ' +
-                  'renaming account: ', err);
-                $scope.error = true;
-                $scope.loading = false;
-                return;
-              }
-
-              // Re-login
-              // TODO implement refresh/relogin in ID.
-              id.login({
-                username: $scope.username,
-                password: $scope.password
-              }, function (err) {
-                if (err) {
-                  console.log('client: account tab: error while ' +
-                    'logging user in: ', err);
-                  $scope.error = 'cantlogin';
-                  $scope.loading = false;
-                  return;
-                }
-
-                $scope.success = true;
-                reset();
-              });
-            });
-          }
-        );
-      };
-
-      var reset = function() {
-        $scope.openForm = false;
-        $scope.username = '';
-        $scope.password = '';
-        $scope.showPassword = true;
-        $scope.loading = false;
-        $scope.error = false;
-
-        if ($scope.renameForm) {
-          $scope.renameForm.$setPristine(true);
-        }
-
-      };
-
-      reset();
-      $scope.success = false;
-
+AccountTab.prototype.angular = function(module) {
+  module.controller('AccountCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+      if (!$routeParams.route) {
+        $routeParams.route = 'public';
+      }
     }]
   );
 };
