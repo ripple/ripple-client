bbc3d6edeaa4a34dddd584395b59085330c1c0b1
Hovhannes Kuloghlyan <chokoc@gmail.com>
2015-06-27 04:06:26 +0400

    [FEAT] Logout: Logout URL for IDS autologout
    https://RT.URL/#/logout?redirect_to=URL logs out user
    from RT and RTB and redirects back to the mentioned URL

--- src/js/services/id.service.js
+++ src/js/services/id.service.js
@@ -486,7 +486,7 @@ module.factory(
     $scope.$on('$routeChangeStart', function(ev, next) {
       if (!$scope.loginStatus) {
         var tab = next.tabName;
-        var allTabs = ['login', '404', 'privacypolicy', 'tou'];
+        var allTabs = ['login', 'logout', '404', 'privacypolicy', 'tou'];
         if (allTabs.indexOf(tab) !== -1) {
           $scope.showLogin = false;
         } else {
