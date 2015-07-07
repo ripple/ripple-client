3fa61943bc9bf8c202686a5973725f65a968e649
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-07-07 23:35:51 +0300

    [TASK] Add GateHub gateway (RT-3486)
    Add GateHub gateway to the Gateways list

--- src/js/tabs/eur.controller.js
+++ src/js/tabs/eur.controller.js
@@ -21,6 +21,10 @@ EurTab.prototype.angular = function (module)
         $scope.showInstructions = !$scope.showInstructions;
       };
 
+      $scope.toggle_gatehub_instructions = function() {
+        $scope.show3Instructions = !$scope.show3Instructions;
+      }
+
       $scope.save_account = function() {
         $scope.loading = true;
 
@@ -29,7 +33,7 @@ EurTab.prototype.angular = function (module)
             {reference_date: new Date(+new Date() + 5*60000)}
             );
 
-        amount.set_issuer("rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q");
+        amount.set_issuer('rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q');
 
         if (!amount.is_valid()) {
           // Invalid amount. Indicates a bug in one of the validators.
@@ -91,20 +95,19 @@ EurTab.prototype.angular = function (module)
               $scope.tx_result = 'failed';
               break;
             case 'tel':
-              $scope.tx_result = "local";
+              $scope.tx_result = 'local';
               break;
             case 'tep':
               console.warn('Unhandled engine status encountered!');
           }
-          if ($scope.tx_result=="cleared"){
+          if ($scope.tx_result === 'cleared') {
             $scope.eurConnected = true;
             $scope.showInstructions = true;
-
           }
           console.log($scope.tx_result);
         }
 
-        keychain.requestSecret(id.account, id.username, function (err, secret) {
+        keychain.requestSecret(id.account, id.username, function(err, secret) {
           // XXX Error handling
           if (err) {
             $scope.loading = false;
@@ -116,18 +119,103 @@ EurTab.prototype.angular = function (module)
 
           tx.secret(secret);
           tx.submit();
-
-
         });
-
       };
-      $scope.$watch('lines', function () {
-        if($scope.lines['rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2qEUR']){
-          $scope.eurConnected = true;
+
+      $scope.save_gatehub_account = function() {
+        $scope.eur3loading = true;
+
+        var amount = ripple.Amount.from_human(
+            Options.gateway_max_limit + ' ' + 'EUR',
+            {reference_date: new Date(+new Date() + 5*60000)}
+            );
+
+        amount.set_issuer('rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq');
+
+        if (!amount.is_valid()) {
+          // Invalid amount. Indicates a bug in one of the validators.
+          console.log('Invalid amount');
+          return;
         }
-        else {
-          $scope.eurConnected = false;
+
+        var tx = $network.remote.transaction();
+
+        // Add memo to tx
+        tx.addMemo('client', 'rt' + $scope.version);
+
+        // Flags
+        tx
+          .rippleLineSet(id.account, amount)
+          .setFlags('NoRipple')
+          .on('proposed', function(res) {
+            $scope.$apply(function() {
+              setEngineStatus(res, false);
+            });
+          })
+          .on('success', function(res) {
+            $scope.$apply(function() {
+              setEngineStatus(res, true);
+
+              $scope.eur3loading = false;
+            });
+          })
+          .on('error', function(res) {
+            setEngineStatus(res, false);
+            console.log('error', res);
+            setImmediate(function() {
+              $scope.$apply(function () {
+                $scope.eur3loading = false;
+              });
+            });
+          });
+
+        function setEngineStatus(res, accepted) {
+          $scope.engine3_result = res.engine_result;
+          $scope.engine3_result_message = res.engine_result_message;
+          $scope.engine3_status_accepted = accepted;
+
+          switch (res.engine_result.slice(0, 3)) {
+            case 'tes':
+              $scope.tx3_result = accepted ? 'cleared' : 'pending';
+              break;
+            case 'tem':
+              $scope.tx3_result = 'malformed';
+              break;
+            case 'ter':
+              $scope.tx3_result = 'failed';
+              break;
+            case 'tec':
+              $scope.tx3_result = 'failed';
+              break;
+            case 'tel':
+              $scope.tx3_result = 'local';
+              break;
+            case 'tep':
+              console.warn('Unhandled engine status encountered!');
+          }
+          if ($scope.tx3_result === 'cleared') {
+            $scope.eur3Connected = true;
+            $scope.show3Instructions = true;
+          }
+          console.log($scope.tx_result);
         }
+
+        keychain.requestSecret(id.account, id.username, function(err, secret) {
+          // XXX Error handling
+          if (err) {
+            $scope.eur3loading = false;
+            console.log(err);
+            return;
+          }
+
+          tx.secret(secret);
+          tx.submit();
+        });
+      };
+
+      $scope.$watch('lines', function() {
+        $scope.eurConnected = !!$scope.lines.rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2qEUR;
+        $scope.eur3Connected = !!$scope.lines.rhub8VRN55s94qWKDv6jmDy1pUykJzF3wqEUR;
       }, true);
 
       // User should be notified if the reserve is insufficient to add a gateway
